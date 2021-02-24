5f546ed8fee5a35c8e75cef575517cef11086c4b
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-24 17:55:52 +0100

    fixed some little bugs: <p> tags and 0 padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import episodes from  './episodes.json'
+import episodes from './episodes.json'
 
 // const myJSON = data;
 // const episodes = JSON.parse(myJSON);
@@ -24,26 +24,46 @@ interface EpisodeProps {
 }
 
 
-function EpisodeMaker (props: EpisodeProps) {
+function EpisodeMaker(props: EpisodeProps) {
+  
   return (
-    <div className='body'> 
-      <div className='title'> <h3> {`${props.name} - S0${props.season}E0${props.number}`} </h3> </div>
+    <div className='body'>
+      <div className='title'> 
+      <h3> {props.name} - S{props.season<10 ? `0${props.season}` : props.season}E{props.number<10 ? `0${props.number}` : props.number} </h3> 
+      </div>
       <img src={props.image.medium} alt="medium-sized caption of each episode"></img>
-      <p>{props.summary}</p>
-      
-  </div>)
+      <p>{props.summary.replace(/<p>|<\/p>/g, '')}</p>
+
+    </div>)
 }
 
 function App() {
-  console.log(episodes)
+  const [search, setSearch] = useState('')
+  console.log("the state:", search)
+  const [episodeList, setEpisodeList] = useState(episodes)
+  
   return (
     <div className="App">
+
+      <h1 className='header'> Game of Thrones </h1>
       
-      <h1 className='header'> Game of Thrones </h1> 
-      
-      {episodes.map(EpisodeMaker) }
-    
-    <footer className = "footer"> <strong>The data on this website has been provided by <a href=" https://tvmaze.com/"> TvMaze.com </a> </strong> </footer>
+      <div className='search-bar'> <strong> Search for episodes here: </strong>
+        <input value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+            let input: string = event.target.value;
+            console.log("the variable:", input);
+            setEpisodeList(episodes.filter((episode) => {
+              return (
+                episode.name.toLowerCase().includes(input.toLowerCase()) ||
+                episode.summary.toLowerCase().includes(input.toLowerCase())
+            )}))
+
+          }}></input></div>
+
+      <div className='episodes'> {episodeList.map(EpisodeMaker)} </div>
+
+      <footer className="footer"> <strong>The data on this website has been provided by <a href=" https://tvmaze.com/"> TvMaze.com </a> </strong> </footer>
     </div>
   );
 }
