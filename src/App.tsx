07ba6c8081ef5b0fb773f7904df8a4b453a0e3b7
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-24 12:11:40 +0100

    used flexbox on the css

--- src/App.tsx
+++ src/App.tsx
@@ -27,9 +27,10 @@ interface EpisodeProps {
 function EpisodeMaker (props: EpisodeProps) {
   return (
     <div className='body'> 
-      <h3> {`${props.name} - S0${props.season}E0${props.number}`} </h3>
+      <div className='title'> <h3> {`${props.name} - S0${props.season}E0${props.number}`} </h3> </div>
       <img src={props.image.medium} alt="medium-sized caption of each episode"></img>
-      <p> {props.summary} </p>
+      <p>{props.summary}</p>
+      
   </div>)
 }
 
@@ -37,7 +38,12 @@ function App() {
   console.log(episodes)
   return (
     <div className="App">
+      
+      <h1 className='header'> Game of Thrones </h1> 
+      
       {episodes.map(EpisodeMaker) }
+    
+    <footer className = "footer"> <strong>The data on this website has been provided by <a href=" https://tvmaze.com/"> TvMaze.com </a> </strong> </footer>
     </div>
   );
 }
