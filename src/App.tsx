dc2aaf6f208a5817f1e9595ce8f19a2ded8a7267
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-24 11:14:58 +0100

    basic app working: map function that goes through my episodes array

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import episodes from  './episodes.json'
+
+// const myJSON = data;
+// const episodes = JSON.parse(myJSON);
+interface EpisodeProps {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+
+function EpisodeMaker (props: EpisodeProps) {
+  return (
+    <div className='body'> 
+      <h3> {`${props.name} - S0${props.season}E0${props.number}`} </h3>
+      <img src={props.image.medium} alt="medium-sized caption of each episode"></img>
+      <p> {props.summary} </p>
+  </div>)
+}
 
 function App() {
+  console.log(episodes)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {episodes.map(EpisodeMaker) }
     </div>
   );
 }
